45f2651409411e0044c8065b33f84b66836fec94
João Neto <joaonetoprivado2001@gmail.com>
2023-10-31 09:27:54 -0300

    feat: login de usuario v1

--- app_rede_ufpi/models.py
+++ app_rede_ufpi/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+
+    email = models.CharField(
+        max_length=255
+    )
+    senha = models.CharField(
+        max_length=100
+    )
+    matricula = models.CharField(
+        max_length=11
+    )
