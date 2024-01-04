d8ba2c8ab48f5eaf27b9010abec9da30e6debd4a
João Neto <joaonetoprivado2001@gmail.com>
2024-01-04 00:38:34 -0300

    ignore

--- project_rede_ufpi/urls.py
+++ project_rede_ufpi/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from app_rede_ufpi import views
 from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
